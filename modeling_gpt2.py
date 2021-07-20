d797d0e58707fd1b4f64e4a46025db6dfabad691
Tucker Darby <tucker@MBP.local>
2021-07-20 14:18:26 -0600

    past_key_values for gpt2

--- modeling_gpt2.py
+++ modeling_gpt2.py
@@ -551,7 +551,7 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
     def forward(
         self,
         input_ids=None,
-        past=None,
+        past_key_values=None,
         attention_mask=None,
         token_type_ids=None,
         position_ids=None,
@@ -604,7 +604,7 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
         """
         transformer_outputs = self.transformer(
             input_ids,
-            past=past,
+            past_key_values=past_key_values,
             attention_mask=attention_mask,
             token_type_ids=token_type_ids,
             position_ids=position_ids,
